0352d3076cffea481d79e6886f6c37da2ef4aec4
Elena Liubimova <elena.liubimova3@gmail.com>
2023-02-15 23:26:21 +0300

    feat: добавить swiper

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './scss/index.scss';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  
+  modules: [Navigation, Pagination],
+
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true,
+  keyboard: true,
+});
